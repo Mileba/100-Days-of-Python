a650dba0057e3924a8ed7d673f558b4eb753cdf8
Mileba <mykellforbes@gmail.com>
2021-10-21 08:12:42 +0100

    Created TODO 4

--- Day 16/main.py
+++ Day 16/main.py
@@ -4,6 +4,7 @@ from money_machine import MoneyMachine
 
 money_machine = MoneyMachine()
 coffee_machine = CoffeeMaker()
+# menu_item = MenuItem()
 menu = Menu()
 available_menu = menu.get_items()
 is_machine_off = False
@@ -18,7 +19,14 @@ while not is_machine_off:
     elif user_prompt == "report":
         coffee_machine.report()
         money_machine.report()
-
     # TODO 4: "Handles what happens when user enters espresso"
+    elif user_prompt == "espresso":
+        espresso = menu.find_drink("espresso")
+        payment = money_machine.make_payment(espresso.cost)
+        if payment:
+            print("coffee")
+            if coffee_machine.is_resource_sufficient(espresso):
+                coffee_machine.make_coffee(espresso)
+
     # TODO 5: "Handles what happens when user enters latte"
     # TODO 6: "Handles what happens when user enters cappuccino"
