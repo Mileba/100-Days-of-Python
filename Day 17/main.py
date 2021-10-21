7325371a7f50cb9bf438bdf900914f143599bdcf
Mileba <mykellforbes@gmail.com>
2021-10-21 15:48:01 +0100

    Day 17

---
+++ Day 17/main.py
@@ -0,0 +1,15 @@
+class User:
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
+        self.follower = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.follower += 1
+        self.following += 1
+
+
+user_1 = User(1, "mileba")
+
+print(user_1.username)
