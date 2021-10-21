dba68bcf61f4924a692fc25a5ae73c22f2db737e
Mileba <mykellforbes@gmail.com>
2021-10-21 06:38:01 +0100

    Day 16

---
+++ Day 16/main.py
@@ -0,0 +1,8 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+#TODO 1: "Prompt User on what they want"
+#TODO 2: "Turn off the coffee machine by pressing 'off'"
+#TODO 3: "Print Report: Should print report"
+#TODO 4: "Add more functionalities later"
