1648bbe6947f96e18454afb69c91125e0357ea97
Mileba <mykellforbes@gmail.com>
2021-10-29 19:47:29 +0100

    Created a snake class

--- Day 23/main.py
+++ Day 23/main.py
@@ -10,18 +10,14 @@ screen.tracer(0)
 screen.listen()
 
 
-snake = Snake().snake_body()
+snake = Snake()
 
 game_is_on = True
 
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    for body in range(len(snake) - 1, 0, -1):
-        new_x = snake[body - 1].xcor()
-        new_y = snake[body - 1].ycor()
-        snake[body].goto(new_x, new_y)
+    snake.move()
 
-    snake[0].forward(20)
-#
+
 screen.exitonclick()
--- Day 23/snake.py
+++ Day 23/snake.py
@@ -3,15 +3,22 @@ from turtle import Turtle
 
 class Snake:
     def __init__(self):
-        pass
+        self.snake_body = []
+        self.create_snake()
 
-    def snake_body(self):
-        snake_body = []
-
+    def create_snake(self):
         for number in range(3):
             position = number * -20
             tim = Turtle('square')
             tim.color('white')
             tim.penup()
             tim.setposition(position, 0)
-            snake_body.append(tim)
+            self.snake_body.append(tim)
+
+    def move(self):
+        for body in range(len(self.snake_body) - 1, 0, -1):
+            new_x = self.snake_body[body - 1].xcor()
+            new_y = self.snake_body[body - 1].ycor()
+            self.snake_body[body].goto(new_x, new_y)
+
+        self.snake_body[0].forward(20)
