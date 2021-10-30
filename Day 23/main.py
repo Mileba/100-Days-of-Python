bb69c88795bced9dfb764063451e0d4afcf6f360
Mileba <mykellforbes@gmail.com>
2021-10-30 12:39:38 +0100

    Added event listeners

--- Day 23/main.py
+++ Day 23/main.py
@@ -7,11 +7,17 @@ screen.setup(600, 600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
-screen.listen()
+
 
 
 snake = Snake()
 
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 game_is_on = True
 
 while game_is_on:
--- Day 23/snake.py
+++ Day 23/snake.py
@@ -1,10 +1,16 @@
 from turtle import Turtle
 
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 360
+
 
 class Snake:
     def __init__(self):
         self.snake_body = []
         self.create_snake()
+        self.head = self.snake_body[0]
 
     def create_snake(self):
         for number in range(3):
@@ -21,4 +27,20 @@ class Snake:
             new_y = self.snake_body[body - 1].ycor()
             self.snake_body[body].goto(new_x, new_y)
 
-        self.snake_body[0].forward(20)
+        self.head.forward(20)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
