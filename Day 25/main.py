b09ba2653414f490684a0b540d94abe45005852c
Mileba <mykellforbes@gmail.com>
2021-11-02 11:33:10 +0100

    Initailize the player object

--- Day 25/main.py
+++ Day 25/main.py
@@ -7,9 +7,13 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+screen.listen()
+
+player = Player()
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    screen.onkey(player.move, "Up")
 
--- Day 25/player.py
+++ Day 25/player.py
@@ -1,8 +1,21 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__(shape="turtle")
+        self.penup()
+        self.setheading(90)
+        self.goto(STARTING_POSITION)
 
+    def move(self):
+        self.forward(MOVE_DISTANCE)
+        self.reset_player()
+
+    def reset_player(self):
+        if self.ycor() > FINISH_LINE_Y:
+            self.goto(STARTING_POSITION)
